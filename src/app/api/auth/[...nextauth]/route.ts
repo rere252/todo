35cba4a2ed8db2d86eea825962cba472f6b5e214
Rene Juuse <rene.juuse@nortal.com>
2023-08-27 19:07:31 +0300

    fix: Illegal nextauth route options export

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,5 @@
 import NextAuth from 'next-auth/next';
-import GithubProvider from "next-auth/providers/github"
-
-export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string
-    })
-  ]
-};
+import {authOptions} from './options';
 
 const handler = NextAuth(authOptions);
 
